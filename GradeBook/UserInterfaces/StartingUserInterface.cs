e1344c9dcf46db66dcc5c90639f7b15ac6be3867
Zordock <62941009+Zordock@users.noreply.github.com>
2020-03-31 12:13:46 -0400

    Update StartingUserInterface.cs

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -42,6 +42,7 @@ namespace GradeBook.UserInterfaces
             var name = parts[1];
             var type = parts[2].ToLower();
             BaseGradeBook gradeBook;
+            
             if (type == "standard")
                 gradeBook = new StandardGradeBook(name);
             else if (type == "ranked")
