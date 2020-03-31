7a401fbabfcd20b243afac29a0730d464ad8f130
Zordock <62941009+Zordock@users.noreply.github.com>
2020-03-31 12:24:02 -0400

    Update StartingUserInterface.cs

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -41,15 +41,15 @@ namespace GradeBook.UserInterfaces
             }
             var name = parts[1];
             var type = parts[2].ToLower();
+
             BaseGradeBook gradeBook;
-            
             if (type == "standard")
                 gradeBook = new StandardGradeBook(name);
             if (type == "ranked")
                 gradeBook = new RankedGradeBook(name);
             else
             {
-                Console.WriteLine("{0} is not a supported type of gradebook.");
+                Console.WriteLine("{0} is not a supported type of gradebook, please try again", type);
                 return;
             }
             
